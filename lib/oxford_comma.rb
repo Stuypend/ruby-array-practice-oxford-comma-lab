8030d592a7f4fa65979fb4aa3b8c0d38f8e6b9a4
stuypend <andre@stuypend.com>
2021-02-22 11:17:03 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
+  if (array.size == 2)
+    return "#{array[0]} and #{array[1]}"
+  end
 
+  result = array.shift
+
+  while(array.size > 0)
+    if(array.size > 1)
+      result += ", #{array.shift}"
+    else
+      result += ", and #{array.shift}"
+    end
+
+  end
+  return result
 end
+
+x = ["1", "2"]
+puts oxford_comma(x)
